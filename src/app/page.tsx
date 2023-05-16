cdcccd891f3c67a1c5be49f18f52417aaecc8b95
Felipe Scherer <felipescherer25@gmail.com>
2023-05-15 22:59:07 -0300

    :sparkles: feat: problem page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import { ProblemList } from '@/features/ProblemList'
+// import { ProblemList } from '@/features/ProblemList'
+import { ProblemPage } from '@/features/ProblemPage'
 
 export default function Home() {
-  return <ProblemList />
+  return (
+    <>
+      {/* <ProblemList /> */}
+      <ProblemPage />
+    </>
+  )
 }
--- src/features/ProblemPage/index.tsx
+++ src/features/ProblemPage/index.tsx
@@ -1,7 +1,26 @@
 export function ProblemPage() {
   return (
-    <div>
-      <div>dasdasd</div>
+    <div className="m-5 flex flex-col gap-10 font-bold">
+      <div className="flex flex-col">
+        <h2 className="text-3xl">#01 - Multiples of 3 or 5</h2>
+        <span className="text-sm">Created at 13/05/2023</span>
+        <span className="text-sm">Solved at 13/05/2023</span>
+      </div>
+
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis
+          tincidunt eros. Duis elementum viverra pulvinar. Curabitur ultrices
+          sem sed risus condimentum varius ut laoreet metus. Nulla varius, orci
+          porta aliquet placerat, elit massa vulputate purus, vitae elementum
+          tortor odio sed urna. Morbi sagittis, mi ut fermentum gravida, lacus
+          lacus luctus tellus, nec volutpat massa tellus id turpis. In aliquet
+          enim a nisl imperdiet, eu molestie lectus laoreet. Sed vitae mauris
+          luctus, viverra justo vel, luctus ante. Vivamus at fringilla lorem.
+        </p>
+
+        <div />
+      </div>
     </div>
   )
 }
