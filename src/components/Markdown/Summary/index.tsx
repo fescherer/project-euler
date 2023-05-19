d8256b8b6ea3ca4b59f5b3ab0fe2c490a9959a8e
Felipe Scherer <felipescherer25@gmail.com>
2023-05-19 16:05:50 -0300

    :sparkles: feat: new components

--- src/components/Markdown/Summary/index.tsx
+++ src/components/Markdown/Summary/index.tsx
@@ -1,3 +1,12 @@
+'use client'
+
+import { CaretDown } from 'phosphor-react'
+
 export function SummaryMarkdown({ children }: any) {
-  return <summary className="cursor-pointer">{children}</summary>
+  return (
+    <summary className="group group flex cursor-pointer items-center justify-between text-xl">
+      {children}
+      <CaretDown className="group-open:text-primary" />
+    </summary>
+  )
 }
