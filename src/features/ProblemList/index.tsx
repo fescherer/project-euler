af1c14ed1c0983f2d77471c66822757ab0319123
Felipe Scherer <felipescherer25@gmail.com>
2023-06-24 22:40:25 -0300

    :sparkles: feat: search input

--- src/features/ProblemList/index.tsx
+++ src/features/ProblemList/index.tsx
@@ -2,12 +2,30 @@
 
 import { ProjectMetadata } from '@/@types/Metadata'
 import { ProblemCard } from '@/components/ProblemCard'
+import { useSearchInputContext } from '@/context/search.context'
 
 type ProblemListProps = {
   data: ProjectMetadata[]
 }
 
+function getFilteredData(data: ProjectMetadata[], filter: string) {
+  if (!filter) return data
+  else {
+    const rg = new RegExp(`^(?=.*\\b${filter}).*$`, 'gmi')
+    return data.filter(
+      (item) =>
+        item.title.match(rg) ||
+        item.id.toString().match(rg) ||
+        item.slug.match(rg)
+    )
+  }
+}
+
 export function ProblemList({ data }: ProblemListProps) {
+  const { searchInput } = useSearchInputContext()
+
+  const filteredData = getFilteredData(data, searchInput)
+
   return (
     <div className="m-5 flex flex-col gap-10">
       <div>
@@ -19,7 +37,7 @@ export function ProblemList({ data }: ProblemListProps) {
       </div>
 
       <div className="flex flex-wrap justify-center gap-5">
-        {data.map((item) => (
+        {filteredData.map((item) => (
           <ProblemCard key={item.id} card={item} />
         ))}
       </div>
